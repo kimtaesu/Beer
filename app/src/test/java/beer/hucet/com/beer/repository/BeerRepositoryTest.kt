c49d454ac83feb543a95c62cf5a1b8e46bb67643
taesu <kimtaesoo188@gmail.com>
2017-12-05 13:57:18 +0900

    UnitTest : BeerRepositoryTest.kt

---
+++ app/src/test/java/beer/hucet/com/beer/repository/BeerRepositoryTest.kt
@@ -0,0 +1,51 @@
+package beer.hucet.com.beer.repository
+
+import beer.hucet.com.beer.api.PunkApi
+import com.nhaarman.mockito_kotlin.whenever
+import io.reactivex.Flowable
+import org.amshove.kluent.mock
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.subject.SubjectSpek
+
+/**
+ * Created by taesu on 2017-12-05.
+ */
+class BeerRepositoryTest : SubjectSpek<BeerRepository>({
+
+    val punkApi by memoized {
+        mock<PunkApi>()
+    }
+    given("BeerRepository")
+    {
+        subject {
+            BeerRepository(punkApi)
+        }
+
+        beforeEachTest {
+            whenever(punkApi.getPagingBeer(1, 1)).thenReturn(Flowable.just(listOf()))
+        }
+        on("State [Complete]")
+        {
+
+            val testSubscribe = subject.getPagingBeer(1, 1).test()
+            it("Assert complete, noErrors")
+            {
+                testSubscribe.assertComplete()
+                testSubscribe.assertNoErrors()
+            }
+        }
+        on("State [Error]")
+        {
+
+            whenever(punkApi.getPagingBeer(1, 1))
+                    .thenReturn(Flowable.just(1).map { throw RuntimeException() })
+            val testSubscribe = subject.getPagingBeer(1, 1).test()
+            it("Assert noComplete, error") {
+                testSubscribe.assertError(RuntimeException::class.java)
+                testSubscribe.assertNotComplete()
+            }
+        }
+    }
+})
